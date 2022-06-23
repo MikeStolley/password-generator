fba5e5784be6ec6ec198fc954b93ae0ff41d17cd
Mike Stolley <stolleywilliam@yahoo.com>
2022-06-23 18:10:57 -0400

    Added comments for code written out

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,17 @@
 // Assignment code here
-var totalPassword = ""
+
+//Variables assigned to all numerical and character values
 var lower = "abcdefghijklmnopqrstuvwxyz"
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
 var special = "!@#$%^&*()?!"
+var finalPassword = ""
+var totalPassword = ""
 
 
 function generatePassword() {
 
+//If statement discussed in class, returns to the beginning if no value is given
 var length = prompt("How long is your password?")
 console.log(length);
   if (length < 8 || length > 128) {
@@ -15,33 +19,33 @@ console.log(length);
     return generatePassword();
   };
 
+//window statements getting information for final password generation
 var textUpperCase = window.confirm("Upper case letters in your password?")
-  if (textUpperCase) {
+  if (textUpperCase = confirm) {
     totalPassword += upper;
   };
 
 var textLowerCase = window.confirm("Lower case letters in your password?")
-  if (textLowerCase) {
+  if (textLowerCase = confirm) {
     totalPassword += lower;
   };
 
 var textNumbers = window.confirm("Numbers in your password?")
-  if (textNumbers) {
+  if (textNumbers = confirm) {
     totalPassword += number;
   };
 
 var textSpecCh = window.confirm("Special Characters in your password?")
-  if (textSpecCh) {
+  if (textSpecCh = confirm) {
     totalPassword += special;
   };
 
-  var finalPassword = ''
-
+//For statement using charAt, in order to hold a character of data for the total password that is generated above.
   for (var i = 0; i <= length; i++) {
     finalPassword += totalPassword.charAt(Math.floor(Math.random() * totalPassword.length));
   }
 
-
+//return the final generated password
 return finalPassword;
 
 };
